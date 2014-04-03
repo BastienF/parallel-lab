ae70921c8f84588ba84d258e3c24cb0f4c9d8a26
BastienF <fiorentino.bastien@gmail.com>
2014-04-03 15:47:45 +0200

    Add Jetty

--- AutoEnvOracle7/launch.sh
+++ AutoEnvOracle7/launch.sh
@@ -2,63 +2,24 @@
 
 vagrant up > vagrant.log
 launchDate=`date '+%d-%m-%Y.%T'`
-ansible-playbook -i hosts -l server deploy_webapp.yml --private-key=~/.vagrant.d/insecure_private_key
+server=$1
+if [ "$server" != "tomcat" ] && [ "$server" != "jetty" ]; then 
+	server="tomcat"
+fi
+echo "Server setted at" $server
+ansible-playbook -i hosts -l server deploy_webapp.yml --private-key=~/.vagrant.d/insecure_private_key --extra-vars 'server='$server
 launch () {
-        ansible-playbook -i hosts -l server run_once.yml --private-key=~/.vagrant.d/insecure_private_key --extra-vars 'iterations='$3' implementation='$1' users='$2' launchDate='$launchDate' duration='$4
-        ansible-playbook -i hosts -l gatling run_once.yml --private-key=~/.vagrant.d/insecure_private_key --extra-vars 'iterations='$3' implementation='$1' users='$2' launchDate='$launchDate' duration='$4
+        ansible-playbook -i hosts -l server run_once.yml --private-key=~/.vagrant.d/insecure_private_key --extra-vars 'iterations='$3' implementation='$1' users='$2' launchDate='$launchDate' duration='$4' server='$server
+        ansible-playbook -i hosts -l gatling run_once.yml --private-key=~/.vagrant.d/insecure_private_key --extra-vars 'iterations='$3' implementation='$1' users='$2' launchDate='$launchDate' duration='$4' server='$server
 }
 
 launch_all() {
         launch akka $1 $2 $3;
         launch naive $1 $2 $3;
-        launch mono $1 $2 $3;
-        launch executor $1 $2 $3;
-        launch pool $1 $2 $3;
+       	launch mono $1 $2 $3;
+       	launch executor $1 $2 $3;
+       	launch pool $1 $2 $3;
 }
 
 launch_all 15 5000000 5;
-<<COMMENT
-launch_all 15 1000 120;
-launch_all 15 1500 120;
-launch_all 15 2000 120;
-launch_all 15 2500 120;
-launch_all 15 3500 120;
-launch_all 15 5000 120;
-launch_all 15 6000 120;
-launch_all 15 9000 120;
-launch_all 15 12000 120;
-launch_all 15 15000 120;
-launch_all 15 20000 120;
-launch_all 15 25000 120;
-launch_all 15 37500 120;
-launch_all 15 50000 120;
-launch_all 15 65000 120;
-launch_all 15 90000 120;
-launch_all 15 118000 120;
-launch_all 15 150000 120;
-launch_all 15 200000 120;
-launch_all 15 280000 120;
-launch_all 15 375000 120;
-launch_all 15 500000 120;
-launch_all 15 667000 120;
-launch_all 15 890000 120;
-launch_all 15 1180000 120;
-launch_all 15 1580000 120;
-launch_all 15 2100000 120;
-launch_all 15 2810000 120;
-launch_all 15 3750000 120;
-launch_all 15 5000000 120;
-#launch_all 100 93750 30;
-#launch_all 100 125000 30;
-#launch_all 100 187500 30;
-#launch_all 100 250000 30;
-#launch_all 100 325000 30;
-#launch_all 100 500000 30;
-#launch_all 100 750000 30;
-#launch_all 100 1000000 30;
-#launch_all 100 1500000 30;
-#launch_all 100 2000000 30;
-#launch_all 100 3000000 30;
-#launch_all 100 4000000 30;
-COMMENT
 vagrant halt >> vagrant.log;
