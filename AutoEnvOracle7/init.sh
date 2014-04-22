da6363b2b45e651f06aea86202e2c47047c833e0
BastienF <fiorentino.bastien@gmail.com>
2014-04-22 18:51:15 +0200

    Update environnement to java 8

--- AutoEnvOracle7/init.sh
+++ AutoEnvOracle7/init.sh
@@ -1,5 +1,6 @@
 ssh-keygen -R [127.0.0.1]:12347
 ssh-keygen -R [127.0.0.1]:23457
+ssh-keygen -R [127.0.0.1]:23458
 sudo chown `whoami`:staff /etc/exports
 vagrant up
 ansible-playbook -i hosts provisioning.yml --private-key=~/.vagrant.d/insecure_private_key
