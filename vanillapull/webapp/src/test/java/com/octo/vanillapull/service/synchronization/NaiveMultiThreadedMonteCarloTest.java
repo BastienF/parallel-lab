321c25c85c2c33f5db61c40895e853e90d486142
Henri Tremblay <henri.tremblay@gmail.com>
2014-06-12 14:22:51 +0200

    Put tests back

--- vanillapull/webapp/src/test/java/com/octo/vanillapull/service/synchronization/NaiveMultiThreadedMonteCarloTest.java
+++ vanillapull/webapp/src/test/java/com/octo/vanillapull/service/synchronization/NaiveMultiThreadedMonteCarloTest.java
@@ -16,7 +16,7 @@ public class NaiveMultiThreadedMonteCarloTest {
     c.interestRate = 0.015;
 
     // insert into INSTRUMENT(symbol, label, SPOT, VOLATILITY, VARIATION) values('BNP','BNP Paribas', 45.04, 1, 0.89);
-    double actual = 28.2;//c.calculatePrice(90, 45.04, 17, 1);
+    double actual = c.calculatePrice(90, 45.04, 17, 1);
     assertEquals(28.2, actual, 0.1);
   }
 }
